81138f6283598269bcf8c95540a5fb312623bb63
Your Name01~pickypou <ludo.spysschaert@orange.fr>
2023-06-22 08:57:28 +0200

    ajout des images aux annonces

---
+++ src/Controller/ImagesController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Annonces;
+use App\Entity\Images;
+use App\Form\ImagesType;
+use App\Service\UploaderService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class ImagesController extends AbstractController
+{
+    private $entityManager;
+    private $imagesDirectory;
+    public function __construct(EntityManagerInterface $entityManager, ParameterBagInterface $parameterBag)
+    {
+        $this->entityManager = $entityManager;
+        $this->imagesDirectory = $parameterBag->get('images_directory');
+    }
+    #[Route('/images/{id}', name: 'app_images')]
+    public function index(
+        Request $request,
+        Annonces $annonce,
+        UploaderService $uploaderService
+    ): Response {
+        $image = new Images();
+        $image->setAnnonce($annonce);
+
+        $imageForm = $this->createForm(ImagesType::class, $image);
+
+        $imageForm->handleRequest($request);
+
+        if ($imageForm->isSubmitted() && $imageForm->isValid()) {
+            $imgUne = $imageForm->get('imgUne')->getData();
+            $imgDeux = $imageForm->get('imgDeux')->getData();
+            $imgTrois = $imageForm->get('imgTrois')->getData();
+            $imgQuatre = $imageForm->get('imgQuatre')->getData();
+            $imgCinq = $imageForm->get('imgCinq')->getData();
+            $imgSix = $imageForm->get('imgSix')->getData();
+            if ($imgUne) {
+                $image->setimgUne($uploaderService->uploadFile($imgUne, $this->imagesDirectory));
+            }
+            if ($imgDeux) {
+                $image->setImgDeux($uploaderService->uploadFile($imgDeux, $this->imagesDirectory));
+            }
+            if ($imgTrois) {
+                $image->setImgTrois($uploaderService->uploadFile($imgTrois, $this->imagesDirectory));
+            }
+            if ($imgQuatre) {
+                $image->setImgQuatre($uploaderService->uploadFile($imgQuatre, $this->imagesDirectory));
+            }
+            if ($imgCinq) {
+                $image->setImgCinq($uploaderService->uploadFile($imgCinq, $this->imagesDirectory));
+            }
+            if ($imgSix) {
+                $image->setImgSix($uploaderService->uploadFile($imgSix, $this->imagesDirectory));
+            }
+            $image->setAnnonce($annonce);
+
+            $this->entityManager->persist($image);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_annonces_list');
+        }
+        return $this->render('images/index.html.twig', [
+            'imageForm' => $imageForm->createView(),
+            'annonce' => $annonce
+        ]);
+    }
+}
---
+++ src/Form/ImagesType.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonces;
+use App\Entity\Images;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class ImagesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('imgUne',FileType::class, [
+                'label' => 'Image une)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/gif',
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid Image',
+                    ])
+                ],
+            ])
+            ->add('imgDeux', FileType::class,[
+                'label' => 'Image une)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/gif',
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid Image',
+                    ])
+                ],
+            ])
+            ->add('imgTrois', FileType::class,[
+                'label' => 'Image une)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/gif',
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid Image',
+                    ])
+                ],])
+            ->add('imgQuatre', FileType::class,[
+                'label' => 'Image une)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/gif',
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid Image',
+                    ])
+                ],])
+            ->add('imgCinq', FileType::class,[
+                'label' => 'Image une)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/gif',
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid Image',
+                    ])
+                ],])
+            ->add('imgSix', FileType::class,[
+                'label' => 'Image une)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/gif',
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid Image',
+                    ])
+                ],])
+            ->add('annonce', EntityType::class, [
+                'class' => Annonces::class, // Entité cible de la relation
+                'label' => 'Annonce',
+                'choice_label' => 'id', // Propriété de l'entité à afficher dans le formulaire
+                'disabled' => true, // Rend le champ en lecture seule
+                'required' => true, // Le champ est obligatoire
+                ])
+
+                ->add('submit',SubmitType::class,[
+                    'label'=>'ajouté les image'
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Images::class,
+        ]);
+    }
+}
