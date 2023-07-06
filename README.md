9ad9ff2f4133375dc2012289aca35c564337be8e
Your Name01~pickypou <ludo.spysschaert@orange.fr>
2023-07-06 14:07:24 +0200

    creation de ADMIN lors de la migration

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Garage V. Parrot
 
-- lien du projet
-pour la partie back-end: https://github.com/pickypou/garage-back.git
-pour la partie front-end: https://github.com/pickypou/garage-front.git
+- lien du projet: https://github.com/pickypou/garage-back.git
 
--installer les 
+### En local
+
+-git clone https://github.com/pickypou/garage-back.git
+
+- ce rendre a la racine du projet
+- faire:
+- composer install
+- symfony console doctrine:database:create
+- symfony console doctrine:migrations;migrate
+- symfony serve
+
+### Création d'un administrateur
+
+- crée un utilisateur
+
+- dans la base de donées
+
+sur la colonne roles entré ["ROLE_ADMIN"]
+
--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
-use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,32 +16,52 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class RegisterController extends AbstractController
 {
     private $entityManager;
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
-    #[Route('/register', name: 'app_register')]
-    public function index(Request $request, UserPasswordHasherInterface $userPassword): Response
-    {
-        $user = new User();
 
-        $registerForm = $this->createForm(RegisterType::class,$user);
-       $registerForm->remove('roles');
+    public function __construct(ManagerRegistry $registry)
+    {
+        $this->entityManager = $registry->getManager();
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPassword): Response
+    {
+        $userRepository = $this->entityManager->getRepository(User::class);
+        $users = $userRepository->findAll();
+
+        if (empty($users)) {
+            // Aucun utilisateur dans la base de données, créer un compte administrateur automatiquement
+            $user = new User();
+            $user->setEmail('ludo@ludo.com');
+            $user->setPassword($userPassword->hashPassword($user, 'motdepasse'));
+            $user->setRoles(["ROLE_ADMIN"]);
+            $user->setFristname('ludovic');
+            $user->setLastname('spysschaert');
+            // Définir d'autres attributs de l'utilisateur si nécessaire (rôles, autorisations, etc.)
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            // Rediriger vers la page d'accueil ou une autre page appropriée après la création du compte administrateur
+            return $this->redirectToRoute('app_home');
+        }
+
+        $user = new User();
+        $registerForm = $this->createForm(RegisterType::class, $user);
+        $registerForm->remove('roles');
         $registerForm->handleRequest($request);
 
         if ($registerForm->isSubmitted() && $registerForm->isValid()) {
-           $user = $registerForm->getData();
+            $password = $userPassword->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
 
-           $password = $userPassword->hashPassword($user, $user->getPassword());
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
 
-           $user->setPassword($password);
-
-           $this->entityManager->persist($user);
-           $this->entityManager->flush();
-           return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_home');
         }
+
         return $this->render('register/index.html.twig', [
-           'registerForm'=>$registerForm->createView(),
+            'registerForm' => $registerForm->createView(),
         ]);
     }
 }
