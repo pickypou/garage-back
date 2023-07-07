8f0734a0a586157f679b1deb0826bc65422ec7aa
Your Name01~pickypou <ludo.spysschaert@orange.fr>
2023-07-07 05:23:06 +0200

    quelque modifications

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,46 +4,27 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
-use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-//#[IsGranted('ROLE_ADMIN')]
+
 class RegisterController extends AbstractController
 {
     private $entityManager;
 
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->entityManager = $registry->getManager();
+        $this->entityManager = $entityManager;
     }
 
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPassword): Response
     {
-        $userRepository = $this->entityManager->getRepository(User::class);
-        $users = $userRepository->findAll();
-
-        if (empty($users)) {
-            // Aucun utilisateur dans la base de données, créer un compte administrateur automatiquement
-            $user = new User();
-            $user->setEmail('ludo@ludo.com');
-            $user->setPassword($userPassword->hashPassword($user, 'motdepasse'));
-            $user->setRoles(["ROLE_ADMIN"]);
-            $user->setFristname('ludovic');
-            $user->setLastname('spysschaert');
-            // Définir d'autres attributs de l'utilisateur si nécessaire (rôles, autorisations, etc.)
-
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
-
-            // Rediriger vers la page d'accueil ou une autre page appropriée après la création du compte administrateur
-            return $this->redirectToRoute('app_home');
-        }
+       
 
         $user = new User();
         $registerForm = $this->createForm(RegisterType::class, $user);
